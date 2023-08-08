d4f2c5a1ac253c7c59f799eebfb21e5e978b9dbe
edgarmoya <edgarcmoya@gmail.com>
2023-08-08 12:27:38 -0400

    fix: Toast maxWidth

--- frontend/src/utils/toastUtils.js
+++ frontend/src/utils/toastUtils.js
@@ -3,11 +3,19 @@ import { toast } from "react-hot-toast";
 export const showSuccessToast = (message) => {
   toast.success(message, {
     position: "center-top",
+    style: {
+      maxWidth: "450px",
+      //background: theme === "dark" ? "#333" : "#fff",
+      //color: theme === "dark" ? "#fff" : "#333",
+    },
   });
 };
 
 export const showErrorToast = (message) => {
   toast.error(message, {
     position: "center-top",
+    style: {
+      maxWidth: "450px",
+    },
   });
 };
