959755c18d9c8f9d0f299ff296ba248b543e72c6
edgarmoya <edgarcmoya@gmail.com>
2025-01-31 12:02:40 -0500

    feat: add fixed provinces and municipalities insertion during migration

--- locations/apps.py
+++ locations/apps.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
 
-
 class LocationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'locations'
     verbose_name = 'Administrar Ubicaciones'
+
+    def ready(self) -> None:
+        import locations.signals
